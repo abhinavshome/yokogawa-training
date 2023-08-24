dd807c7d92bad9667f7fd8a83b33fe5a484274d1
Kumar Abhinav <kabhinav.iitg@gmail.com>
2023-08-24 10:42:18 +0530

    aa

--- basic-html-css-js/app.ts
+++ basic-html-css-js/app.ts
@@ -1,12 +1,13 @@
 console.log("Hello World from console");
 document.getElementById("t").innerHTML = "Welcome home!";
+
 //Variables and Data Types
-var username = "Abhinav";
+let username: string = "Abhinav";
 console.log(username);
 console.log(typeof username);
-var num = 2;
+let num: number = 2;
 // num = "David"; //TS Error
 console.log(num);
 console.log(typeof num);
-var age = 23;
+const age = 23;
 // age = 90; //Error
