03ba9ffc35db83165e7985f36f22adf384af4324
Kumar Abhinav <kabhinav.iitg@gmail.com>
2023-08-24 11:08:16 +0530

    aa

--- basic-html-css-js/app.js
+++ basic-html-css-js/app.js
@@ -1,5 +1,5 @@
 console.log("Hello World from console");
-document.getElementById("t").innerHTML = "Welcome home!";
+// document.getElementById("t").innerHTML = "Welcome home!";
 //Variables and Data Types
 var username = "Abhinav";
 console.log(username);
@@ -9,4 +9,32 @@ var num = 2;
 console.log(num);
 console.log(typeof num);
 var age = 23;
-// age = 90; //Error
+console.log("Hello");
+var isMinor;
+isMinor = false;
+// isMinor = 0; //TS Error
+//Classes and Objects
+var Car = /** @class */ (function () {
+    function Car(numOfWheels, cost) {
+        this.numOfWheels = numOfWheels;
+        this.cost = cost;
+    }
+    Car.prototype.start = function () {
+        console.log("The car with " +
+            this.numOfWheels +
+            " and cost " +
+            this.cost +
+            " has started");
+    };
+    return Car;
+}());
+var car = new Car(4, 2200);
+car.start();
+console.log(car.constructor);
+// we can create objects directly, without a class
+var car2 = {
+    model: "Honda",
+    maxSpeed: 220,
+};
+console.log(car2.model);
+console.log(car2.constructor);
--- basic-html-css-js/app.ts
+++ basic-html-css-js/app.ts
@@ -1,5 +1,5 @@
 console.log("Hello World from console");
-document.getElementById("t").innerHTML = "Welcome home!";
+// document.getElementById("t").innerHTML = "Welcome home!";
 
 //Variables and Data Types
 let username: string = "Abhinav";
@@ -10,4 +10,41 @@ let num: number = 2;
 console.log(num);
 console.log(typeof num);
 const age = 23;
-// age = 90; //Error
+console.log("Hello");
+
+let isMinor: boolean;
+isMinor = false;
+// isMinor = 0; //TS Error
+
+//Classes and Objects
+class Car {
+  numOfWheels: number;
+  private cost: number;
+
+  constructor(numOfWheels, cost) {
+    this.numOfWheels = numOfWheels;
+    this.cost = cost;
+  }
+
+  public start() {
+    console.log(
+      "The car with " +
+        this.numOfWheels +
+        " and cost " +
+        this.cost +
+        " has started"
+    );
+  }
+}
+const car = new Car(4, 2200);
+car.start();
+console.log(car.constructor);
+
+// we can create objects directly, without a class
+let car2 = {
+  model: "Honda",
+  maxSpeed: 220,
+};
+
+console.log(car2.model);
+console.log(car2.constructor);
