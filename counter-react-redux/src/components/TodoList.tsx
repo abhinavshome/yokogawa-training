990e9114914b179759fda406bce3d95b0a9bc9bd
Kumar Abhinav <kabhinav.iitg@gmail.com>
2023-09-05 14:53:53 +0530

    aa

--- counter-react-redux/src/components/TodoList.tsx
+++ counter-react-redux/src/components/TodoList.tsx
@@ -1,7 +1,6 @@
 import { useDispatch, useSelector } from "react-redux";
 import {
   todoListSelector,
-  todosSelector,
   totalTodosSelector,
 } from "../redux/reducers/todosReducer";
 import { useRef } from "react";
