85440cdac0df719ffdfe93d31f18c2f90b65c030
Kumar Abhinav <kabhinav.iitg@gmail.com>
2023-09-14 11:31:45 +0530

    aa

--- player-management-system/src/pages/AddPlayer.tsx
+++ player-management-system/src/pages/AddPlayer.tsx
@@ -1,18 +1,20 @@
 import axios from "axios";
-import { useRef } from "react";
+import { useState } from "react";
 import Config from "../config";
 import { useNavigate } from "react-router-dom";
-import { Player } from "../types";
 
 function AddPlayer() {
-  const playerForm = useRef<HTMLFormElement | undefined>(undefined);
+  const [name, setName] = useState("");
+  const [age, setAge] = useState(0);
+
   const navigate = useNavigate();
 
   const handleSubmit = async (e) => {
     e.preventDefault();
-    const data = new FormData(playerForm.current);
-    const newPlayer: Player = Object.fromEntries(data.entries());
-    newPlayer.age = parseInt(newPlayer.age);
+    const newPlayer = {
+      name: name,
+      age: age,
+    };
     await axios.post(Config.apiUrl, newPlayer);
     navigate("/listing");
   };
@@ -20,14 +22,26 @@ function AddPlayer() {
   return (
     <div className="player-form">
       <div className="title">Add a player</div>
-      <form onSubmit={handleSubmit} ref={playerForm}>
+      Name: {name} <br />
+      Age: {age}
+      <form onSubmit={handleSubmit}>
         <div>
           <label>Name</label>
-          <input name="name" type="text" />
+          <input
+            name="name"
+            type="text"
+            onChange={(e) => {
+              setName(e.target.value);
+            }}
+          />
         </div>
         <div>
           <label>Age</label>
-          <input name="age" type="text" />
+          <input
+            name="age"
+            type="text"
+            onChange={(e) => setAge(parseInt(e.target.value))}
+          />
         </div>
         <div>
           <button type="submit">Submit</button>
